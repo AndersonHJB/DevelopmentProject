7f00caa9d3f6f4953873155036ad8b630ba5c0c0
AndersonHJB <cleland1432803776@icloud.com>
2023-04-16 21:16:13 +0800

    添加用户头像

--- FlaskChatRoom/app.py
+++ FlaskChatRoom/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+import os
+import random
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-
+# from gevent import monkey
+# monkey.patch_all()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'aiyuechuang'
+# socketio = SocketIO(app, async_mode='gevent')
 socketio = SocketIO(app)
+
+
+def get_random_avatar():
+    avatars_dir = 'static/images/Head_picture'
+    avatars = os.listdir(avatars_dir)
+    avatar_file = random.choice(avatars)
+    # print(avatars)
+    # print(avatar_file)
+    return os.path.join(avatars_dir, avatar_file)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    avatar_url = get_random_avatar()
+    return render_template('index.html', avatar_url=avatar_url)
 
 
 @socketio.on('send_message')
@@ -17,4 +31,7 @@ def handle_send_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=12345)
+    # socketio.run(app, host='0.0.0.0', port=12345)
+    socketio.run(app, host='0.0.0.0', port=8000)
+    # socketio.run(app, host='localhost', port=12345)
+    # socketio.run(app, host='192.168.31.155', port=12345)
