f45639ae240134362f4abe6c60802d415f4444f1
AndersonHJB <cleland1432803776@icloud.com>
2023-04-16 22:43:33 +0800

    实现登陆，数据存储

--- FlaskChatRoom/app.py
+++ FlaskChatRoom/app.py
@@ -1,37 +1,81 @@
+from gevent import monkey
+
+monkey.patch_all()
+
 import os
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
-from gevent import monkey
-monkey.patch_all()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'aiyuechuang'
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 socketio = SocketIO(app, async_mode='gevent')
-# socketio = SocketIO(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     avatar_url = db.Column(db.String(120), nullable=False)
+
+
+# db.create_all()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    avatar_url = db.Column(db.String(120), nullable=False)
+
+
+# with app.app_context():
+#     db.create_all()
 
 
 def get_random_avatar():
     avatars_dir = 'static/images/Head_picture'
     avatars = os.listdir(avatars_dir)
     avatar_file = random.choice(avatars)
-    # print(avatars)
-    # print(avatar_file)
     return os.path.join(avatars_dir, avatar_file)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        avatar_url = request.form['avatar_url']
+        user = User(name=name, avatar_url=avatar_url)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index', user_id=user.id))
+    avatars = [os.path.join('static/images/Head_picture', f) for f in os.listdir('static/images/Head_picture')]
+    random.shuffle(avatars)
+    return render_template('login.html', avatars=avatars)
 
 
 @app.route('/')
 def index():
-    avatar_url = get_random_avatar()
-    return render_template('index.html', avatar_url=avatar_url)
+    user_id = request.args.get('user_id')
+    if user_id is None:
+        return redirect(url_for('login'))
+    user = User.query.get(user_id)
+    if user is None:
+        return redirect(url_for('login'))
+    return render_template('index.html', user=user)
 
 
 @socketio.on('send_message')
 def handle_send_message(data):
-    emit('receive_message', data, broadcast=True)
+    user_id = data['user_id']
+    user = User.query.get(user_id)
+    if user is not None:
+        emit('receive_message', {'name': user.name, 'avatar_url': user.avatar_url, 'message': data['message']},
+             broadcast=True)
 
 
 if __name__ == '__main__':
-    # socketio.run(app, host='0.0.0.0', port=12345)
-    socketio.run(app, host='0.0.0.0', port=8000)
-    # socketio.run(app, host='localhost', port=12345)
-    # socketio.run(app, host='192.168.31.155', port=12345)
+    with app.app_context():
+        db.create_all()
+    socketio.run(app, host='0.0.0.0', port=8000, websocket='gevent')
